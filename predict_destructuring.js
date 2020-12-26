d0f8d6a3bf3d8eed0c3d3f2e34ce4e94bd4ad0de
Ricky <rickdhanota@gmail.com>
2020-12-25 23:20:25 -0800

    added problem 2

--- predict_destructuring.js
+++ predict_destructuring.js
@@ -1,3 +1,4 @@
+//Problem 1
 const cars = ['Tesla', 'Mercedes', 'Honda']
 const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
@@ -5,3 +6,14 @@ const [ ,otherRandomCar ] = cars
 console.log(randomCar) //prints the first car in cars
 console.log(otherRandomCar) //prints the second car in cars because the first is skipped 
 
+//Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+//console.log(name); 
+//This line throws an error because the variable doesnt exist, it is not defined
+console.log(otherName); //We save the value of name into otherName
